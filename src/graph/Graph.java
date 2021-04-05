2264d23921e23a9094d9826e9d95c7b2a385ecc4
Debsishu Pal <pauldebsishu2002@gmail.com>
2021-04-06 03:06:09 +0530

    Cycle Detect Algo Add

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -93,6 +93,64 @@ public class Graph {
     }
   }
 
+  public boolean isCycleUndirected() {
+    boolean visited[] = new boolean[this.v];
+    for(int i = 0; i < this.v; i++) {
+      if(visited[i] == false) {
+        if(checkCycleDFSUndirected(i, -1, visited)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  private boolean checkCycleDFSUndirected(int s, int p, boolean[] visited) {
+    visited[s] = true;
+    Iterator<Integer> it = adj[s].iterator();
+    while(it.hasNext()) {
+      int i = it.next();
+      if(visited[i] == false) {
+        if(checkCycleDFSUndirected(i, s, visited) == true) {
+          return true;
+        }
+      } else if(i != p || i == s) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean isCycleDirected() {
+    boolean visited[] = new boolean[this.v];
+    // this keeps a track of if the vertice is already in the recursive call stack or not
+    boolean isRecur[] = new boolean[this.v]; 
+    for(int i = 0; i < this.v; i++) {
+      if(visited[i] == false) {
+        if(checkCycleDFSDirected(i, visited, isRecur)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  private boolean checkCycleDFSDirected(int s, boolean[] visited, boolean[] isRecur) {
+    visited[s] = true;
+    isRecur[s] = true;
+    Iterator<Integer> it = adj[s].iterator();
+    while(it.hasNext()) {
+      int i = it.next();
+      if(visited[i] == false && checkCycleDFSDirected(i, visited, isRecur)) {
+        return true;
+      } else if(isRecur[i] == true && i == s) {
+        return true;
+      }
+    }
+    isRecur[s] = false;
+    return false;
+  }
+
   public static void main(String[] args) {
     Graph g = new Graph(6);
     // g.addEdge(0, 1);
