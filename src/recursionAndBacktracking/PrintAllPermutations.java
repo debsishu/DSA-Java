0e7f1e71dbaabb17ee8084f29058baf9a18533b8
Debsishu Pal <pauldebsishu2002@gmail.com>
2021-05-04 15:41:26 +0530

    Program Update

--- src/recursionAndBacktracking/PrintAllPermutations.java
+++ src/recursionAndBacktracking/PrintAllPermutations.java
@@ -1,15 +1,23 @@
 package recursionAndBacktracking;
 
+import java.util.*;
+
 public class PrintAllPermutations {
+  // This is a geeksforgeeks problem
   public static void main(String[] args) {
     String str = "ABC";
     int n = str.length();
     permute(str, 0, n - 1);
+    Collections.sort(ll);
+    System.out.println(ll);
   }
 
+  static ArrayList<String> ll = new ArrayList<>();
+
   static void permute(String str, int l, int r) {
+    // Base case
     if (l == r) {
-      System.out.print(str + " ");
+      ll.add(str);
     } else {
       for (int i = l; i <= r; i++) {
         str = swap(str, l, i);
@@ -19,6 +27,7 @@ public class PrintAllPermutations {
     }
   }
 
+  // This is for swapping the character positions
   static String swap(String s, int i, int j) {
     char temp;
     char[] charArray = s.toCharArray();
