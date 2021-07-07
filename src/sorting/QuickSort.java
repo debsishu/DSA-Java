4df0d15242cb658018b669992e80fcaab03d870c
Debsishu Pal <pauldebsishu2002@gmail.com>
2021-07-08 01:16:13 +0530

    QuickSort Modify

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -2,7 +2,7 @@ package sorting;
 
 public class QuickSort {
 	public static void main(String[] args) {
-		int arr[] = { 2, 8, 7, 1, 3, 5, 6, 4 };
+		int arr[] = { 2, 4, 1, 3, 5, 9 };
 		quickSort(arr, 0, arr.length - 1);
 		for (int e : arr) {
 			System.out.print(e + " ");
@@ -12,7 +12,8 @@ public class QuickSort {
 
 	static void quickSort(int arr[], int low, int high) {
 		if (high > low) {
-			int pivot = partition(arr, low, high);
+			// int pivot = partition(arr, low, high);
+			int pivot = partitionMod(arr, low, high);
 			quickSort(arr, low, pivot - 1);
 			quickSort(arr, pivot + 1, high);
 		}
@@ -43,6 +44,21 @@ public class QuickSort {
 		int temp = arr[low];
 		arr[low] = arr[high];
 		arr[high] = temp;
+	}
+
+	// Another implementation
+	static int partitionMod(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int pIndex = low;
+
+		for (int i = low; i < high; i++) {
+			if (arr[i] <= pivot) {
+				swap(arr, i, pIndex);
+				pIndex++;
+			}
+		}
+		swap(arr, pIndex, high);
+		return pIndex;
 	}
 
 }
